823b30e16d5d35963e5f705449f7fc8ce24c21ec
Vladimir Zhevagin <vladimir.zhevagin@yandex.ru>
2021-01-23 21:49:24 +0300

    по условию

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int score = 100;
-        int  refill = 100;
+        int  refill = 1100;
         int bonus = 0;
-        if (refill >= 1000) {
+
+        if (refill > 1000) {
             bonus = refill / 100;
-            int new_score = bonus + score + refill;
-            System.out.println("Ваш счет = " + new_score);
+            score = bonus + score + refill;
+            System.out.println("Ваш счет = " + score);
             System.out.println("Ваш бонус = " + bonus);
         } else {
-            int new_score = score + refill;
-            System.out.println("Ваш счет = " + new_score);
-            System.out.println("Ваш бонус = " + bonus + " 'Пополните счет на 1000 рублей, для получения бонусов!'");
+            score = score + refill;
+            System.out.println("Ваш счет = " + score);
+            System.out.println("Ваш бонус = " + bonus + " 'Пополните счет на 1100 рублей, для получения бонусов!'");
         }
 
     }
